9c5fc66d1f2aeee8b0dda8422a948895b9f60178
Jason Zhou <zhoujc999@gmail.com>
2020-12-02 09:46:05 +0800

    Edit assignment

--- Assignment/mnist.py
+++ Assignment/mnist.py
@@ -24,22 +24,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         ###############################################
         # Part (a)
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        
         ###############################################
 
     def forward(self, x):
         ###############################################
-        # Part (a)
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        # Part (b)
+
         ###############################################
         return F.log_softmax(x, dim=1)
 
@@ -127,10 +118,7 @@ def train(args):
             data, target = data.to(device), target.to(device)
             ###############################################
             # Part (c)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target)
-            loss.backward()
+
             ###############################################
             if is_distributed and not use_cuda:
                 # average gradients manually for multi-machine cpu case only
@@ -153,10 +141,7 @@ def test(model, test_loader, device):
             data, target = data.to(device), target.to(device)
             ###############################################
             # Part (d)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()
-            pred = output.max(1, keepdim=True)[1]
-            correct += pred.eq(target.view_as(pred)).sum().item()
+
             ###############################################
 
     test_loss /= len(test_loader.dataset)
